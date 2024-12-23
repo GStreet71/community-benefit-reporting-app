ca577c8c0388b25721a0276aae7411ae828c5ddd
Gregory Harris <144550630+GStreet71@users.noreply.github.com>
2024-12-23 03:35:40 -0600

    Created Login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import TextField from '@mui/material/TextField';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
@@ -6,16 +6,19 @@ import CardActions from '@mui/material/CardActions';
 import Button from '@mui/material/Button';
 import CardHeader from '@mui/material/CardHeader';
 import { useTheme } from '@mui/material/styles'; 
-import { tokens } from '../theme'; // Adjust path to your theme
-import { useNavigate } from 'react-router-dom'; // Import useNavigate
-import { useAuth } from './AuthContext'; // Adjust based on your AuthContext location
+import { ColorModeContext, tokens } from '../theme';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from './AuthContext';
+import IconButton from '@mui/material/IconButton';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
 
 const Login = () => {
   const theme = useTheme(); 
-  const colors = tokens(theme.palette.mode); 
-
-  const { login } = useAuth(); // Get the login function from context
-  const navigate = useNavigate(); // Initialize the navigator
+  const colors = tokens(theme.palette.mode);
+  const { login } = useAuth();
+  const navigate = useNavigate();
+  const { toggleColorMode } = useContext(ColorModeContext);
 
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -31,11 +34,13 @@ const Login = () => {
     if (username === 'abc@email.com' && password === 'password') {
       setError(false);
       setHelperText('Login Successfully');
-      login(); // Call login function from the Auth context
-      navigate('/index'); // Redirect to the index page after successful login
+      console.log('Login Successfully');
+      login();
+      navigate('/dashboard');
     } else {
       setError(true);
       setHelperText('Incorrect username or password');
+      console.log('Incorrect username or password');
       setUsername('');
       setPassword('');
     }
@@ -48,7 +53,15 @@ const Login = () => {
   };
 
   return (
-    <form
+    <div
+    style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      background: `${colors.primary[400]}`
+    }}>
+      <form
       noValidate
       autoComplete="off"
       sx={{
@@ -59,15 +72,15 @@ const Login = () => {
         justifyContent: 'center',
         alignItems: 'center'
       }}
-    >
+      >
       <Card m="10px" sx={{ padding: '10px' }}>
         <CardHeader
           title="Login"
           sx={{
-            backgroundColor: colors.primary[400],
-            color: "#6870fa",
+            backgroundColor: colors.blueAccent[500],
+            color: colors.primary[900],
             textAlign: "center",
-            padding: "20px",
+            padding: "20px"
           }}
         />
         <CardContent>
@@ -80,7 +93,7 @@ const Login = () => {
             placeholder="Username"
             margin="normal"
             onChange={(e) => setUsername(e.target.value)}
-            onKeyPress={handleKeyPress}
+            onKeyDown={handleKeyPress}
             helperText={error ? helperText : ''}
             sx={{
               marginBottom: 2,
@@ -98,7 +111,7 @@ const Login = () => {
             placeholder="Password"
             margin="normal"
             onChange={(e) => setPassword(e.target.value)}
-            onKeyPress={handleKeyPress}
+            onKeyDown={handleKeyPress}
             helperText={error ? helperText : ''}
             sx={{
               '& .MuiInputBase-root': {
@@ -108,24 +121,39 @@ const Login = () => {
           />
         </CardContent>
         <CardActions>
-          <Button
-            variant="contained"
-            size="large"
-            color="secondary"
-            onClick={handleLogin}
-            disabled={isButtonDisabled}
-            sx={{
-              marginTop: 2,
-              flexGrow: 1,
-              backgroundColor: colors.greenAccent[500],
-              color: colors.grey[100],
-            }}
-          >
-            Login
-          </Button>
-        </CardActions>
-      </Card>
-    </form>
+        <Button
+        variant="contained"
+        size="large"
+        onClick={handleLogin}
+        sx={{
+        marginTop: 2,
+        flexGrow: 1,
+        backgroundColor: colors.blueAccent[500],
+        color: colors.primary[400],
+          '&:hover': {
+            backgroundColor: colors.blueAccent[600], // Darker shade on hover
+          },
+        }}
+      >
+        Login
+      </Button>
+          </CardActions>
+        </Card>
+      </form>
+
+      {/* Mode Toggle Icon */}
+      <IconButton 
+        onClick={toggleColorMode} 
+        style={{ position: 'absolute', top: 20, right: 100 }}
+      >
+        {theme.palette.mode === "dark" ? (
+          <DarkModeOutlinedIcon />
+        ) : (
+          <LightModeOutlinedIcon />
+        )}
+      </IconButton>
+    </div>
+    
   );
 };
 
